d2098810b926f90554d69af2cf055cee864c9799
nmate98 <nmate9808@gmail.com>
2019-11-09 15:46:40 +0100

    NavHostFragment létrehozva, és login-register, login-wall, és register-wall navigáció hozzáadva a megfelelő gombokhoz/szöveghez.

--- GamersConnect/app/src/main/java/com/unibmi/gamersconnect/LogInFragment.java
+++ GamersConnect/app/src/main/java/com/unibmi/gamersconnect/LogInFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,11 +33,30 @@ public class LogInFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_log_in, container, false);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_logInFragment_to_wallFragment);
+            }
+        });
+        TextView tv = view.findViewById(R.id.offer_text);
+        String str = (String )tv.getText();
+        SpannableString sstr = new SpannableString(str);
+        sstr.setSpan(str,str.length()-10,str.length()-1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ClickableSpan cstr = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Navigation.findNavController(widget).navigate(R.id.action_logInFragment_to_registerFragment);
+            }
+        };
+        sstr.setSpan(cstr,str.length()-10,str.length()-1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setText(sstr);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
-
+        return view ;
     }
-
 
 }
--- GamersConnect/app/src/main/java/com/unibmi/gamersconnect/RegisterFragment.java
+++ GamersConnect/app/src/main/java/com/unibmi/gamersconnect/RegisterFragment.java
@@ -4,10 +4,12 @@ package com.unibmi.gamersconnect;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -24,8 +26,16 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+        Button button = v.findViewById(R.id.register_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_wallFragment);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return v;
     }
 
 }
