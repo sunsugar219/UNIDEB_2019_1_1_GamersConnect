f844c79f5a6321b3ec20ea458628e1115af46d16
sunsugar219 <sspike219@gmail.com>
2019-11-10 18:29:37 +0100

    Options menu added

--- GamersConnect/app/src/main/java/com/unibmi/gamersconnect/MainActivity.java
+++ GamersConnect/app/src/main/java/com/unibmi/gamersconnect/MainActivity.java
@@ -10,6 +10,8 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,9 +41,33 @@ public class MainActivity extends AppCompatActivity {
         tv.setMovementMethod(LinkMovementMethod.getInstance());*/
 
 
-                ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
-                adapterViewPager = new ViewPagerAdapter(this, getSupportFragmentManager());
-                vpPager.setAdapter(adapterViewPager);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+        adapterViewPager = new ViewPagerAdapter(this, getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.itemProfile:
+                Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.itemWall:
+                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.itemContacts:
+                Toast.makeText(getApplicationContext(),"Item 3 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
