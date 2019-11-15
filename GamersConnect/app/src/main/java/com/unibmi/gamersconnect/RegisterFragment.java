8cfec7da4cd659e18f2602b855b9aa57dbd927c4
sunsugar219 <sspike219@gmail.com>
2019-11-15 14:15:30 +0100

    Navigation issues resolved.

--- GamersConnect/app/src/main/java/com/unibmi/gamersconnect/RegisterFragment.java
+++ GamersConnect/app/src/main/java/com/unibmi/gamersconnect/RegisterFragment.java
@@ -10,12 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.unibmi.gamersconnect.database.User;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+    private DatabaseReference mDatabase;
 
 
     public RegisterFragment() {
@@ -26,16 +32,24 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         View v = inflater.inflate(R.layout.fragment_register, container, false);
         Button button = v.findViewById(R.id.register_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_registerFragment_to_wallFragment);
+
+                Navigation.findNavController(v).navigate(R.id.wallFragment);
             }
         });
         // Inflate the layout for this fragment
         return v;
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
     }
 
 }
