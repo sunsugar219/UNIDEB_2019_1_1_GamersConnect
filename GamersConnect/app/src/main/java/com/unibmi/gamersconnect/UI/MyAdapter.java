8cd582cd5429030a9510ba51c95fd8607ea78ddc
sunsugar219 <sspike219@gmail.com>
2019-11-22 13:02:14 +0100

    Started working on RecyclerView adapter

--- GamersConnect/app/src/main/java/com/unibmi/gamersconnect/UI/MyAdapter.java
+++ GamersConnect/app/src/main/java/com/unibmi/gamersconnect/UI/MyAdapter.java
@@ -2,62 +2,93 @@ package com.unibmi.gamersconnect.UI;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.unibmi.gamersconnect.R;
+import com.unibmi.gamersconnect.database.Message;
 
-public class MyAdapter extends RecyclerView.Adapter<TextViewHolder> {
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MessageViewHolder> {
 
 private Context mContext;
-private int[] mTextList;
+private ArrayList<Message> mMessageList;
+    private int mNumberItems;
 
-public MyAdapter(Context mContext, int[] mPlaceList) {
+
+public MyAdapter(Context mContext, ArrayList<Message> mMessageList) {
         this.mContext = mContext;
-        this.mTextList = mTextList;
-        }
-
-@Override
-public TextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_custom_layout,
-        parent, false);
-        return new TextViewHolder(view);
-        }
-
-@Override
-public void onBindViewHolder(final TextViewHolder holder, int position) {
-        holder.mText.setText(mTextList[position]);
-        holder.mText.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View view) {
-        Intent mIntent = new Intent(mContext, DetailFragment.class);
-        mIntent.putExtra("Text", mTextList[holder.getAdapterPosition()]);
-        mContext.startActivity(mIntent);
-        }
-        });
-        }
-
-@Override
-public int getItemCount() {
-        return mTextList.length;
-        }
-        }
-
-
-class TextViewHolder extends RecyclerView.ViewHolder {
-
-    TextView mText;
-
-    public TextViewHolder(View itemView) {
-        super(itemView);
-
-        mText = itemView.findViewById(R.id.tvText);
-    }
+        this.mMessageList = mMessageList;
+        this.mContext = mContext;
 }
 
 
+    // Override our 3 functions
+    // onCreateViewHolder()
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Log.v("onCreateViewHolder", "onCreateViewHolder is called !");
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.list_item_message;
+
+        // Inflate our new item view using a LayoutInflater. It takes the ID of layout in xml.
+        // Then --> inflates or converts this collection of view groups and views.
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+
+        // Set to false, so that the inflated layout will not be
+        // immediately attached to its parent viewgroup.
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, false);
+        MessageViewHolder viewHolder = new MessageViewHolder(view);
+
+        return viewHolder;
+
+    }
+
+
+    //onBindViewHolder()
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+
+        // Get the data model based on position
+
+        Message message = mMessageList.get(position);
+        // Set item views based on your views and data model
+        TextView textView = holder.mMessageVenue;
+        textView.setText(message.getVenue());
+
+    }
+
+    //getItemCount() : returns the mNumberItems var
+    @Override
+    public int getItemCount() {
+        mNumberItems = mMessageList.size();
+        return mNumberItems;
+    }
+
+}
+
+
+class MessageViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView mAuthorImage;
+    TextView mAuthorText, mMessageVenue, mMessageDate, mMessageDescription;
+
+    public MessageViewHolder(View itemView) {
+        super(itemView);
+
+        mAuthorImage = itemView.findViewById(R.id.author_image_in_list);
+        mAuthorText = itemView.findViewById(R.id.author_text_in_list);
+        mMessageVenue = itemView.findViewById(R.id.message_venue_string);
+        mMessageDate = itemView.findViewById(R.id.message_date);
+        mMessageDescription = itemView.findViewById(R.id.message_description);
+    }
+}
 
